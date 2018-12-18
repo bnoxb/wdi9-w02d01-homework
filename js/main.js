24964a155e19e85a663e0ff032cb363c0455d2f9
Robbie <saucy.cooking@gmail.com>
2018-12-17 18:27:15 -0700

    Commit 1 - data types

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log('js is running');
+
+// 1. I would use booleans, and an object for a light switch that can be turned on or off.  A boolean to control the switch then an object with images.
+
+// 2. A string.  A string held in a variable.
+
+// 3. Strings and some booleans and an Object.  Youll need strings to help explain what is happeneing, booleans to help with the functions running all 
+// the bells and whistles of your spaceship, and an Object to put it all in.  Maybe even an array to hold a series of objects each one  handling a different
+// spaceship tech.
+
+// 4. An array and strings.  Youll need an array with strings each one being a classmates name.
+
+// 5. An Array containing objects and strings.  Youll have an Array with objects for each persons name with a string and a location key with a string in it.
+
+// 6. An array with objects and strings.  An array containing objects for each person.  Each object will have a name, location for strings and array
+// for a list of favorite TV shows.
+
